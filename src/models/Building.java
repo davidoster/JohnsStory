f6a2119e96f45a365a32d2c45a30501867bf04a3
George Pasparakis <paspa@hotmail.com>
2020-09-30 19:52:50 +0300

    Initial

---
+++ src/models/Building.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author user
+ */
+ public abstract class Building {
+     MainHouse mainHouse;
+     Garden garden;
+     // door
+    public void someMethod() {
+        System.out.println("sdfsdf");
+    }
+    
+    public abstract void someOtherMethod();
+    abstract void someOtherMethod2();
+}
---
+++ src/models/House.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author user
+ */
+public class House extends Building {
+
+    @Override
+    public void someOtherMethod() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void someOtherMethod2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
